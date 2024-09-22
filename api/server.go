13d89a1523dcaa4a7eb3a30edbfa79623ab83bf5
Nikita Kolchin <n.kolchin@smartway.today>
2024-09-22 03:07:30 +0300

    add cors

--- api/server.go
+++ api/server.go
@@ -30,6 +30,17 @@ func NewServer(
 ) *http.Server {
 
 	router := mux.NewRouter().UseEncodedPath()
+	router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Set CORS headers
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+			// Continue with the request
+			next.ServeHTTP(w, r)
+		})
+	})
 	router.Handle("/v1/auth/login", handlers.Login(logger, ss)).Methods("POST", "OPTIONS")
 
 	api := router.PathPrefix("v1").Subrouter()
