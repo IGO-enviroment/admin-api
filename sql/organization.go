1880ceca042f7bda35886ba23f44eadbfa8d2547
Nikita Kolchin <n.kolchin@smartway.today>
2024-09-22 08:27:54 +0300

    test

--- sql/organization.go
+++ sql/organization.go
@@ -2,6 +2,6 @@ package sql
 
 const GetOrgProfile = `
 select s.id, s.email, s.first_name, s.last_name, s.middle_name, s.phone_number, o.name, o.description, o.inn, o.kpp, o.ogrn, o.place  from public.users s
-join public.organizations o on p.id = s.organization_id                                                            
+join public.organizations o on o.id = s.organization_id                                                            
 where s.email = '%s'
 `
